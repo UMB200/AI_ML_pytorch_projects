710d7c5730f9d2439eaf1923e1b93586a57dbfdc
Umid Baratov <tomodachi200@gmail.com>
2025-06-12 16:23:12 -0400

    Update model_calc_timer.py

--- model_calc_timer.py
+++ model_calc_timer.py
@@ -1,3 +1,7 @@
+# Import necessary libraries and frameworks
+import torch
+from tqdm.auto import tqdm
+
 from timeit import default_timer as timer
 def print_train_time(start: float, end: float, device: torch.device = None):
   """ Prints difference between start and end time """
