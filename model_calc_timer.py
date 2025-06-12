5080e41c377110130ccdbec464b5c7eb46862fa4
Umid Baratov <tomodachi200@gmail.com>
2025-06-12 16:14:59 -0400

    Create model_calc_timer.py

---
+++ model_calc_timer.py
@@ -0,0 +1,6 @@
+from timeit import default_timer as timer
+def print_train_time(start: float, end: float, device: torch.device = None):
+  """ Prints difference between start and end time """
+  total_time = end - start
+  print(f"Train time on {device}: {total_time:.3f} seconds")
+  return total_time
