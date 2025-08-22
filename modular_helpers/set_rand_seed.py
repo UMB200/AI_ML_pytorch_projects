82e73f3309c7524626a3bfb5331e4845d4fa452c
Umid Baratov <tomodachi200@gmail.com>
2025-08-22 12:53:44 -0400

    Update set_rand_seed.py

--- modular_helpers/set_rand_seed.py
+++ modular_helpers/set_rand_seed.py
@@ -1,8 +1,4 @@
 import torch
 def set__rand_seed(seed: int=42):
-  """ set random seed for toech operations
-  Args:
-        seed(int, optional): Random seed to set, default is 42
-  """
   torch.manual_seed(seed)
   torch.cuda.manual_seed(seed)
