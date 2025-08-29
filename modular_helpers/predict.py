d1b60f47bdf8bdca5626ecc54b052bca1e340345
Umid Baratov <tomodachi200@gmail.com>
2025-08-29 15:08:05 -0400

    Update predict.py

--- modular_helpers/predict.py
+++ modular_helpers/predict.py
@@ -2,6 +2,57 @@ import torch
 import torchvision
 import argparse
 import model_builder
+import random
+from PIL import Image
+from typing import List, Tuple
+from torchvision import transforms
+import matplotlib.pyplot as plt
+
+
+def predict_show_image_fn(model: torch.nn.Module,
+                          image_path: str,
+                          class_names: List[str],
+                          image_size: Tuple[int, int] = (224, 224),
+                          transform=None,
+                          device: torch.device = device):
+  """Makes a prediction on a target image and plots the image with its prediction."""
+
+  # 1. load in image
+  target_img = Image.open(image_path)
+
+  # 3. Transform target image
+  if transform is not None:
+    img_transform = transform
+  else:
+    img_transform = transforms.Compose([
+      transforms.Resize(image_size),
+      transforms.ToTensor(),
+      torchvision.models.EfficientNet_B0_Weights.IMAGENET1K_V1.transforms()
+    ])
+
+  # 4. Make sure model is on target device
+  model.to(device)
+
+  # 5. Turn on model evaluation and inference mode
+  model.eval()
+  with torch.inference_mode():
+    #6. Transform and add an extra dimension to image
+    target_img_transform = img_transform(target_img).unsqueeze(dim=0)
+
+    # 7. Make a prediction on image
+    target_img_pred = model(target_img_transform.to(device))
+
+  # 8. Convert logits -> prediction probabilities using torch.softmax
+  target_img_pred_probs = torch.softmax(target_img_pred, dim=1)
+
+  # 9. Convert prediction probabilities -> prediction labels
+  target_img_pred_label = torch.argmax(target_img_pred_probs, dim=1)
+
+  # 10. Plot the image alongside the predicion and prediction probability
+  plt.figure()
+  plt.title(f"Prediction: {class_names[target_img_pred_label]} | Probability: {target_img_pred_probs.max():.0%}")
+  plt.imshow(target_img)
+  plt.axis(False)
 
 # Create a parser
 parser = argparse.ArgumentParser()
