0d16aca1fe84ce582ea0c7f872aeb3f9b3f687b2
Umid Baratov <tomodachi200@gmail.com>
2025-10-02 15:08:10 -0400

    added plot_loss_curves to utils.py

--- modular_helpers/utils.py
+++ modular_helpers/utils.py
@@ -1,5 +1,47 @@
 import torch
+import matplotlib.pyplot as plt
+import numpy as np
+from torch import nn
+import os
+import zipfile
 from pathlib import Path
+import requests
+
+def plot_loss_curves(results):
+  """Plots training curves of a results dictionary.
+
+  Args:
+    results: dictionary
+    results (dict): dictionary containing list of values, e.g.
+            {"train_loss": [...],
+             "train_acc": [...],
+             "test_loss": [...],
+             "test_acc": [...]}
+  """
+  loss_results = results["train_loss"]
+  test_loss_results = results["test_loss"]
+
+  accuracy_results = results["train_acc"]
+  test_accuracy_results = results["test_acc"]
+
+  epochs = range(len(results["train_loss"]))
+  plt.figure(figsize=(15, 7))
+
+  # Plot loss
+  plt.subplot(1, 2, 1)
+  plt.plot(epochs, loss_results, label="train_loss")
+  plt.plot(epochs, test_loss_results, label="test_loss")
+  plt.title("Loss")
+  plt.xlabel("Epochs")
+  plt.legend()
+
+  # Plot accuracy
+  plt.subplot(1, 2, 2)
+  plt.plot(epochs, accuracy_results, label="train_accuracy")
+  plt.plot(epochs, test_accuracy_results, label="test_accuracy")
+  plt.title("Accuracy")
+  plt.xlabel("Epochs")
+  plt.legend()
 
 def save_model(model: torch.nn.Module,
                target_dir: str,
