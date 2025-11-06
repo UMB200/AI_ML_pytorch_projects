9bbdc8addb0ff8d76f60c6ad4e5bc8b75fef9bc7
Umid Baratov <tomodachi200@gmail.com>
2025-11-06 12:40:55 -0500

    Add optional scheduler parameter to train function
    Added optional learning rate scheduler parameter to the training function.

--- helpers/engine.py
+++ helpers/engine.py
@@ -91,6 +91,7 @@ def train(model: torch.nn.Module,
                optimizer: torch.optim.Optimizer,
                loss_fn: torch.nn.Module,
                epochs: int,
+              scheduler: torch.optim.lr_scheduler._LRScheduler = None,
                device: torch.device)-> Dict[str, List]:
   """Trains and tests a PyTorch model.
 
